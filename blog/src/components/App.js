487235e689b6daf6165925d854ad9fe90acc7051
Zaid Rashid <zed182@gmail.com>
2019-08-19 17:30:26 +0200

    Tutorial continuation

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div className="ui container"><PostList /></div>
+    );
 }
 
 export default App;
--- blog/src/index.js
+++ blog/src/index.js
@@ -4,8 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
